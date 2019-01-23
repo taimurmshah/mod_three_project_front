8f1508c465bf9b9190d4083acb425761d7540b27
github username <github email address>
2019-01-23 12:27:01 -0500

    sign-up is working

--- src/index.js
+++ src/index.js
@@ -18,8 +18,34 @@ function putMessages(ul) {
 document.addEventListener("DOMContentLoaded", () => {
 
   const messageUl = document.getElementById("message-ul")
+  const loginForm = document.getElementById("login-form")
+  const signupForm = document.getElementById("signup-form")
+
+  loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+    let userName = e.target.user.value
+
+  })
+
+
+  signupForm.addEventListener("submit", e => {
+    e.preventDefault();
+    let name = e.target.user.value
+    let language = e.target.lang.value
+    fetch("http://localhost:3000/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name, language
+      })
+    })
+  })
 
   putMessages(messageUl)
+
+
 
 
 
