24703c609e6d206c39d3f762030638c212ae8368
jspradac <jspradac@gmail.com>
2019-01-23 14:47:38 -0500

    Sam's front end first push

--- src/index.js
+++ src/index.js
@@ -20,17 +20,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const messageUl = document.getElementById("message-ul")
   const loginForm = document.getElementById("login-form")
   const signupForm = document.getElementById("signup-form")
+  const loginContainer = document.getElementById("login-container")
+  const main = document.getElementById("test")
+  let userName;
 
   loginForm.addEventListener("submit", e => {
     e.preventDefault();
-    let userName = e.target.user.value
-
+    userName = e.target.user.value
+    loginContainer.style.display = "none"
+    main.hidden = false
+    console.log(userName);
   })
 
 
   signupForm.addEventListener("submit", e => {
     e.preventDefault();
-    let name = e.target.user.value
+    userName = e.target.user.value
     let language = e.target.lang.value
     fetch("http://localhost:3000/api/v1/users", {
       method: "POST",
@@ -38,8 +43,12 @@ document.addEventListener("DOMContentLoaded", () => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        name, language
+        name: userName, language
       })
+    })
+    .then(() => {
+      loginContainer.style.display = "none";
+      main.hidden = false;
     })
   })
 
